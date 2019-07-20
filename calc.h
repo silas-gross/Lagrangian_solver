254668bc128cbf65fe832b6dc29db0f04e867e57
silas-gross <skg2131@columbia.edu>
2019-07-20 16:16:37 -0400

    properl setting up git repo

--- calc.h
+++ calc.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #ifndef Calc_H
 #define Calc_H
@@ -21,28 +20,4 @@ private:
 	std::string set_as_derivative(std::string var1, std::string var_2, int degree) //replaces variable 1 with a derivatve of variable 2 of given degree
 };
 
-=======
-#pragma once
-#ifndef Calc_H
-#define Calc_H
-#include <math.h>
-#include <string>
-#include <vector>
-#include "func.h"
-
-
-class calc
-{
-public:
-	Functions::point_function derivate(Functions::point_function pfunc, Functions::symbolic_function sfunc, std::string var, float boundl, float boundu);
-	Functions::point_function integral(std::vector <float> func, bool imp, float bl, float bu);
-	Functions::symbolic_function find_solutions(Functions::symbolic_function func_1, Functions::symbolic_function func_2, std::string var1, std::string var2, int degree);
-	Functions::point_function find_solutions(Functions::point_function func_1, Functions::point_function func_2, std::string var1, std::string var2, int degree);
-private:
-	double localdev(Functions::point_function pfunc, Functions::symbolic_function sfunc, std::vector<float> init, float boundl, float boundu);
-	float Reimansum(std::vector<float> init, float boundl, float boundu, long nsteps);
-	std::string set_as_derivative(std::string var1, std::string var_2, int degree) //replaces variable 1 with a derivatve of variable 2 of given degree
-};
-
->>>>>>> Calc_and_funcs
 #endif	
