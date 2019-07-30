348ff18060d9cf7639079b168042d7af8342e19d
silas-gross <skg2131@columbia.edu>
2019-07-29 22:38:28 -0400

    Minor updates

--- calc.h
+++ calc.h
@@ -15,8 +15,16 @@ public:
 	Functions::symbolic_function find_solutions(Functions::symbolic_function func_1, Functions::symbolic_function func_2, std::string var1, std::string var2, int degree);
 	Functions::point_function find_solutions(Functions::point_function func_1, Functions::point_function func_2, std::string var1, std::string var2, int degree);
 private:
-	double localdev(Functions::point_function pfunc, Functions::symbolic_function sfunc, std::vector<float> init, float boundl, float boundu);
-	float Reimansum(std::vector<float> init, float boundl, float boundu, long nsteps);
+	double localdev(Functions::point_function pfunc, Functions::symbolic_function sfunc, std::vector<float> init, float boundl, float boundu); 
+	/********************************************************************************
+	*              The following describes the purpose of each parameter            *
+	* Parameter 1: Point function representation of 
+	* Parameter 2: Symbolic Function representation 
+	* Parameter 3:
+	* Parameter 4:
+	* Parameter 5:
+	*/
+	float Reimansum(std::vector<float> init, float boundl, float boundu, long nsteps); //
 	std::string set_as_derivative(std::string var1, std::string var_2, int degree) //replaces variable 1 with a derivatve of variable 2 of given degree
 };
 
